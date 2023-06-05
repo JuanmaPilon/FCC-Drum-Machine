ee447fd401847592bc591df12f310adfb798cf8a
unknown <juanmartin.pilon@gmail.com>
2023-06-05 14:20:49 -0300

    Cambie la manera de hacerlo, se me dificulto de la otra manera usando React Sounds por ende trate de seguir otro tutorial donde me fue mas facil lograr lo que queria.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,99 @@
 import React, { useEffect, useState } from "react";
-import DrumPad from "./DrumPad";
-import Sounds from "react-sound"
+import "./App.css";
 
-const App = () => {
-  const [displayText, setDisplayText] = useState("");
+function App() {
+  const [activeKey, setActiveKey] = useState('');
 
- const [audioSources, setAudioSources] = useState ({
-	Q: ".src/sounds/Cev_H2.mp3",
- });
-
-  const handleButtonClick = (text) => {
-	setDisplayText(Text);
-  };
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      console.log(event.key);
+      playSound(event.key.toUpperCase());
+    });
+  }, []);
 
 
-return (
-  <div id="drum-machine">
-    <div id="display">{displayText}</div>
-	<DrumPad id="Q"	text="Q" onClick={() => handleButtonClick("Audio1")}>Q</DrumPad>
-	<DrumPad id="W"	text="W" onClick={() => handleButtonClick("Audio2")}>Q</DrumPad>
-	<DrumPad id="E"	text="E" onClick={() => handleButtonClick("Audio3")}>Q</DrumPad>
-	<DrumPad id="A"	text="A" onClick={() => handleButtonClick("Audio4")}>Q</DrumPad>
-	<DrumPad id="S"	text="S" onClick={() => handleButtonClick("Audio5")}>Q</DrumPad>
-	<DrumPad id="D"	text="D" onClick={() => handleButtonClick("Audio6")}>Q</DrumPad>
-	<DrumPad id="Z"	text="Z" onClick={() => handleButtonClick("Audio7")}>Q</DrumPad>
-	<DrumPad id="X"	text="X" onClick={() => handleButtonClick("Audio8")}>Q</DrumPad>
-	<DrumPad id="C"	text="C" onClick={() => handleButtonClick("Audio9")}>Q</DrumPad>
-  </div>
-	)
-};
+  const drumPads = [
+    {
+      keyCode: 81,
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyCode: 87,
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyCode: 69,
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyCode: 65,
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyCode: 83,
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyCode: 68,
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyCode: 90,
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyCode: 88,
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyCode: 67,
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
+  function playSound(selector) {
+    const audio = document.getElementById(selector);
+    console.log(audio)
+    audio.play();
+    setActiveKey(selector)
+  }
+
+  return (
+    <div className="App">
+      <div id="drum-machine">
+        <div id="display">{activeKey}
+          <div id="drum-pads">
+            {drumPads.map((drumPad) => (
+              <div
+                key={drumPad.src}
+                onClick={() => {
+                  playSound(drumPad.text);
+                }}
+                className="drum-pad"
+                id={drumPad.src}
+              >
+                {drumPad.text}
+                <audio
+                  src={drumPad.src}
+                  className="clip"
+                  id={drumPad.text}
+                ></audio>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default App;
